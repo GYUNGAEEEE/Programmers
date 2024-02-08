42ce3764fec6e2e650a6f7159c2091b85e9311ac
GYUNGAEEEE <158580466+GYUNGAEEEE@users.noreply.github.com>
2024-02-08 11:11:05 +0900

    [level 0] Title: 수 조작하기 2, Time: 694.32 ms, Memory: 398 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(int[] numLog) {
+        String answer = "";
+        int gap = 0;
+        for(int i = 0; i < numLog.length - 1; i++){
+            gap = numLog[i + 1] - numLog[i];
+            switch(gap){
+                case 1:
+                    answer += "w"; break;
+                case -1:
+                    answer += "s"; break;
+                case 10:
+                    answer += "d"; break;
+                case -10:
+                    answer += "a"; break;
+            }
+        }
+        return answer;
+    }
+}
