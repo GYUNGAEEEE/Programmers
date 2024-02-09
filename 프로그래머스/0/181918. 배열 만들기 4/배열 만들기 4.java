642253354fec2004ee08444fbbd783c3c56a3809
GYUNGAEEEE <158580466+GYUNGAEEEE@users.noreply.github.com>
2024-02-09 23:35:20 +0900

    [level 0] Title: 배열 만들기 4, Time: 2.67 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> stkList = new ArrayList<Integer>();
+        int i = 0;
+        while(i < arr.length){
+            if(stkList.size() == 0)
+                stkList.add(arr[i++]);
+            else if(stkList.get(stkList.size() - 1) < arr[i])
+                stkList.add(arr[i++]);
+            else
+                stkList.remove(stkList.size() - 1);
+        }
+        
+        int[] stk = new int[stkList.size()];
+        int idx = 0;
+        for(int temp : stkList)
+            stk[idx++] = temp;
+        return stk;
+    }
+}
