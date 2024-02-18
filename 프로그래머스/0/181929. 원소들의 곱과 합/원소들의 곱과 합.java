c4a610bf1f5b8766d73e99baeb4bb521d85cd7a8
GYUNGAEEEE <158580466+GYUNGAEEEE@users.noreply.github.com>
2024-02-18 10:25:55 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.06 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int[] num_list) {
         int pro = 1;
         int sum = 0;
-        for(int i = 0; i < num_list.length; i++){
+        for(int i = 0; i < num_list.length; i++) {
             pro *= num_list[i];
             sum += num_list[i];
         }
