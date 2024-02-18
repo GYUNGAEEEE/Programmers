a44a7cac3ccab16f32eb2119f0ba2c5e61088f5e
GYUNGAEEEE <158580466+GYUNGAEEEE@users.noreply.github.com>
2024-02-18 11:29:37 +0900

    [level 0] Title: 배열 만들기 2, Time: 1.61 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -1,31 +1,32 @@
 import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
     public int[] solution(int l, int r) {
-        ArrayList<Integer> arr = new ArrayList<Integer>();
+        List<Integer> arrayList = new ArrayList<Integer>();
+        
         for(int i = l; i <= r; i++) {
             int temp = i;
             int checkNum = 0;
             while(true) {
                 checkNum = temp % 10;
                 temp /= 10;
+                
                 if(checkNum != 0 && checkNum != 5)
                     break;
-                else{
-                    if(temp == 0) {
-                        arr.add(i);
+                
+                if(temp == 0) {
+                        arrayList.add(i);
                         break;
-                    }
                 }
             }
         }
         
-        int[] answer = new int[arr.size()];
+        int[] answer = new int[arrayList.size()];
         int idx = 0;
-        for(int num : arr){
-            answer[idx] = num;
-            idx++;
-        }
+        for(int num : arrayList)
+            answer[idx++] = num;
+        
         if(answer.length == 0)
             return new int[]{-1};
         else
