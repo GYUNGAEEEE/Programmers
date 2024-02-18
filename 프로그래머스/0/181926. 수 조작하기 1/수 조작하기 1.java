1f1756aff505b522c5148d56c5c842d4900e59a3
GYUNGAEEEE <158580466+GYUNGAEEEE@users.noreply.github.com>
2024-02-18 10:36:46 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.03 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int n, String control) {
-        for(int i = 0; i < control.length(); i++){
-            switch(control.charAt(i)){
+        for(int i = 0; i < control.length(); i++) {
+            switch(control.charAt(i)) {
                 case 'w':
-                    n++; break;
+                    ++n; break;
                 case 's':
-                    n--; break;
+                    --n; break;
                 case 'd':
                     n += 10; break;
                 case 'a':
