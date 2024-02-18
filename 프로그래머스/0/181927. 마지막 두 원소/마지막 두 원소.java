c73535643fc033c5b8c47b2cc77b0043c66b987c
GYUNGAEEEE <158580466+GYUNGAEEEE@users.noreply.github.com>
2024-02-18 10:34:21 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,12 +1,14 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int size = num_list.length;
-        int[] answer = new int[size + 1];
-        System.arraycopy(num_list, 0, answer, 0, size);
-        if(num_list[size - 1] > num_list[size - 2])
-            answer[size] = num_list[size - 1] - num_list[size - 2];
+        int[] answer = new int[num_list.length + 1];
+        System.arraycopy(num_list, 0, answer, 0, num_list.length);
+        
+        int lastIdx = num_list.length - 1;
+        if(num_list[lastIdx] > num_list[lastIdx - 1])
+            answer[lastIdx + 1] = num_list[lastIdx] - num_list[lastIdx - 1];
         else
-            answer[size] = num_list[size - 1] * 2;
+            answer[lastIdx + 1] = num_list[lastIdx] * 2;
+        
         return answer;
     }
 }
