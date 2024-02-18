2c42fa2cfa34bcdfef848508c92855467766e979
GYUNGAEEEE <158580466+GYUNGAEEEE@users.noreply.github.com>
2024-02-18 11:55:13 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.08 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -1,38 +1,37 @@
 class Solution {
     public int solution(int a, int b, int c, int d) {
-        int[] diceNum = new int[6];
-        diceNum[a - 1]++;
-        diceNum[b - 1]++;
-        diceNum[c - 1]++;
-        diceNum[d - 1]++;
+        int[] diceNum = new int[7];
+        diceNum[a]++;
+        diceNum[b]++;
+        diceNum[c]++;
+        diceNum[d]++;
         
         int p = 0, q = 0, r = 0, s = 0;
-        for(int i = 0; i < diceNum.length; i++) {
-            if(diceNum[i] >= 2 && p == 0) {
-                p = i + 1;
-            } else if(diceNum[i] >= 1 && q == 0) {
-                q = i + 1;
-            } else if(diceNum[i] != 0 && r == 0) { 
-                r = i + 1;
-            } else if(diceNum[i] != 0 && s == 0) {
-                s = i + 1;
-            } else if(diceNum[i] != 0 && p == 0) {
-                p = i + 1;
-            }
+        for(int i = 1; i <= 6; i++) {
+            if(diceNum[i] >= 2 && p == 0)
+                p = i;
+            else if(diceNum[i] >= 1 && q == 0)
+                q = i;
+            else if(diceNum[i] != 0 && r == 0)
+                r = i;
+            else if(diceNum[i] != 0 && s == 0)
+                s = i;
+            else if(diceNum[i] != 0 && p == 0)
+                p = i;
         }
 
-        if(diceNum[p - 1] == 4)
+        if(diceNum[p] == 4)
             return 1111 * p;
-        else if(diceNum[p - 1] == 3)
+        else if(diceNum[p] == 3)
             return (int)Math.pow(10 * p + q, 2);
-        else if(diceNum[q - 1] == 2)
+        else if(diceNum[q] == 2)
             return (p + q) * Math.abs(p - q);
-        else if(diceNum[p - 1] == 2)
+        else if(diceNum[p] == 2)
             return q * r;
         else
-            for(int i = 0; i < diceNum.length; i++) {
+            for(int i = 1; i <= 6; i++) {
                 if(diceNum[i] != 0)
-                    return i + 1;
+                    return i;
             }
         
         return 0;
