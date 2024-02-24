8dbedee50a5e2f13c582da852dce2641a823dfe1
GYUNGAEEEE <158580466+GYUNGAEEEE@users.noreply.github.com>
2024-02-24 19:36:51 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        for(int i = 0; i <= myString.length() - pat.length(); i++) {
+            if(myString.charAt(i) == pat.charAt(0)) {
+                String temp = myString.substring(i, i + pat.length());
+                if(pat.equals(temp))
+                    answer++;
+            }
+        }
+        return answer;
+    }
+}
