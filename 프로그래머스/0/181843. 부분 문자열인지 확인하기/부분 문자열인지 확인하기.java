c65d8df9481d4806afe179e9e38b0011e7c9e41f
GYUNGAEEEE <158580466+GYUNGAEEEE@users.noreply.github.com>
2024-02-29 14:56:07 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String my_string, String target) {
+        if(my_string.indexOf(target) == -1)
+            return 0;
+        else
+            return 1;
+    }
+}
